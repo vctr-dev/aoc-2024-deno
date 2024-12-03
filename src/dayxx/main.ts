b1e70c33f7048fe21d225948d3affd0ac23a1540
Victor Chan <vchan@atlassian.com>
2024-12-03 16:16:00 +1100

    Added template

---
+++ src/dayxx/main.ts
@@ -0,0 +1,12 @@
+export default async function (inputPath: string) {
+  const input = await Deno.readTextFile(inputPath);
+  const parsed = parseInput(input);
+  console.log(parsed);
+  // Part 1
+  // Part 2
+}
+
+function parseInput(input: string) {
+  return input
+    .trim();
+}
