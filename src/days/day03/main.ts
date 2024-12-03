8d72af3c7ed997fc7294d7d8074a6cfdc546c9d3
Victor Chan <vchan@atlassian.com>
2024-12-03 16:15:49 +1100

    Day 3 solutions

--- src/days/day03/main.ts
+++ src/days/day03/main.ts
@@ -1,6 +1,43 @@
-import fs from "node:fs/promises";
-
+import { sum } from "../../utils.ts";
 export default async function (inputPath: string) {
-  const input = await fs.readFile(inputPath, { encoding: "utf-8" });
-  console.log(input);
+  const input = await Deno.readTextFile(inputPath);
+  const parsed = parseInput(input);
+  // Part 1
+  // const regex = /mul\((\d{1,3}),(\d{1,3})\)/mg;
+  // const res = [...parsed.matchAll(regex)].map((v) => ({
+  // a: ~~v[1],
+  // b: ~~v[2],
+  // match: v[0],
+  // }));
+  // console.log(sum(res.map(({ a, b }) => a * b)));
+  // Part 2
+  const regex = /do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/mg;
+  const res = [...parsed.matchAll(regex)].map(([match, a, b]) => ({
+    match,
+    a,
+    b,
+  }));
+
+  console.log(res);
+  let mulEnabled = true;
+  let sum = 0;
+  res.forEach(({ match, a, b }) => {
+    if (match === "do()") {
+      mulEnabled = true;
+      return;
+    }
+    if (match === "don't()") {
+      mulEnabled = false;
+      return;
+    }
+    if (mulEnabled) {
+      sum += ~~a * ~~b;
+    }
+  });
+  console.log(sum);
+}
+
+function parseInput(input: string) {
+  return input
+    .trim();
 }
