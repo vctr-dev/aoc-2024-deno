46ebac05ba51e5260b2eae683366941160a763df
Victor Chan <vchan@atlassian.com>
2024-12-23 21:18:49 +1100

    Clean up day 23

--- src/days/23/2.ts
+++ src/days/23/2.ts
@@ -9,53 +9,44 @@ export default async function (inputPath: string) {
 	);
 	const cluster = findLargestCluster(nodeMap)!;
 	console.log(
-		[...cluster.currentCluster, ...cluster.commonNodes].sort().join(','),
+		[...cluster.cluster, ...cluster.commonNodes].sort().join(','),
 	);
 }
 
 function findLargestCluster(
 	nodeMap: Map<string, Set<string>>,
 ) {
-	const items = [...nodeMap.entries()].filter((v) => v[0].startsWith('t')).map((
-		[k, v],
-	) => ({
-		currentCluster: [k],
-		commonNodes: v,
-	}));
+	const items = [...nodeMap.entries()]
+		.filter((v) => v[0].startsWith('t'))
+		.map(([k, v]) => ({ cluster: [k], commonNodes: v }));
 	const heap = BinaryHeap.from(items, {
 		compare: (a, b) =>
-			b.currentCluster.length + b.commonNodes.size - a.currentCluster.length -
+			b.cluster.length + b.commonNodes.size - a.cluster.length -
 			a.commonNodes.size,
 	});
 
-	// First, for each item, we make a pair of {currentCluster: [], commonNodes: Set<string>}
-	//
-	// Pick the one with the most common nodes + cluster
-	// Find the largest cluster within that common nodes
-	// Collect it and combine it with the current cluster (maybe largest cluster)
-	//
-	// Next, we pick the one with the next most common nodes  size + cluster size
-	// If the common node size plus the cluster size is less than the largest cluster, we confirmed that we have found the largest cluster. Return it.
 	let last: typeof items[number] = items[0];
 	while (heap.length) {
 		const cur = heap.pop()!;
+
 		// Terminating condition
 		if (
-			(cur.commonNodes.size + cur.currentCluster.length) <=
-				last.currentCluster.length
+			(cur.commonNodes.size + cur.cluster.length) <=
+				last.cluster.length
 		) {
 			return last;
 		}
+
 		cur.commonNodes.forEach((node) => {
-			const connections = nodeMap.get(node) || new Set();
-			const commonNodes = cur.commonNodes.intersection(connections).difference(
-				new Set(cur.currentCluster),
-			);
+			const commonNodes = (nodeMap.get(node) || new Set())
+				.intersection(cur.commonNodes)
+				.difference(new Set(cur.cluster));
 			heap.push({
 				commonNodes,
-				currentCluster: cur.currentCluster.toSpliced(0, 0, node),
+				cluster: cur.cluster.toSpliced(0, 0, node),
 			});
 		});
+
 		last = cur;
 	}
 }
