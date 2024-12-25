b1db24af7f48ec3212ea5b945a8da733fa5fc9a7
Victor Chan <vchan@atlassian.com>
2024-12-25 19:01:26 +1100

    Day25 solutions

--- src/days/25/1.ts
+++ src/days/25/1.ts
@@ -1,12 +1,42 @@
 export default async function (inputPath: string) {
 	const input = await Deno.readTextFile(inputPath);
-	const parsed = parseInput(input);
-	console.log(parsed);
-	// Part 1
-	// Part 2
+	const { locks, keys } = parseInput(input);
+	const res = [];
+	for (const key of keys) {
+		for (const lock of locks) {
+			if (key.every((v, i) => v + lock[i] <= 5)) {
+				res.push([lock, key]);
+			}
+		}
+	}
+	console.log(res.length);
+	// schematics: every top row filled, every bottom row empty
+	// locks: top row filled,
+	// keys: are bottom row filled
 }
 
 function parseInput(input: string) {
-	return input
-		.trim();
+	const lockOrKey = input
+		.trim().split('\n\n');
+	const locks = lockOrKey
+		.filter((v) => v.startsWith('#####'))
+		.map((lock) => {
+			return getCode(lock.split('\n').slice(1));
+		});
+	const keys = lockOrKey
+		.filter((v) => v.startsWith('.....'))
+		.map((key) => getCode(key.split('\n').reverse().slice(1)));
+	return { locks, keys };
+}
+
+function getCode(rows: string[]) {
+	const code = Array.from({ length: 5 }, () => 0);
+	rows.forEach((row) =>
+		row.split('').forEach((col, y) => {
+			if (col === '#') {
+				code[y] += 1;
+			}
+		})
+	);
+	return code;
 }
